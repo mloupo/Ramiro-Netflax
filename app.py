78e710aee38064eb0ac8dc9ccd090b25abe831a8
Martin Sergio Lopez <martin.sergio.lopez@gmail.com>
2022-07-02 23:06:28 -0300

    Prueba de conexion e Insert en DB

--- app.py
+++ app.py
@@ -13,15 +13,33 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
-    sql = "INSERT INTO netflax_22068.peliculas(`nombre`, `descripcion`, `imagen`) VALUES ('El Padrino II','El Padrino II (descripcion)','');"
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute(sql)
+    #cursor.execute(sql)
     conn.commit()
     
     return render_template('peliculas/index.html')
 
+@app.route('/create')
+def create():
+    return render_template('peliculas/create.html')
 
+@app.route('/store', methods = ['POST'])
+def store():
+    _nombre = request.form['txtNombre']
+    _desc = request.form['txtDesc']
+    _foto  = request.files['txtImagen']
+    
+    datos =(_nombre, _desc, _foto.filename)
+    sql = "INSERT INTO netflax_22068.peliculas(`nombre`, `descripcion`, `imagen`) VALUES (%s,%s,%s);"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+        
+    return render_template('peliculas/create.html')
+
+# http://127.0.0.1:5000
 if __name__ == '__main__':
     app.run(debug=True)
 
